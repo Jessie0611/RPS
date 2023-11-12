8096a854d2ab5b5ca07ba02c0ba5b74e238b6318
Jessie0611~ <imjustme0611@hotmail.com>
2023-11-12 12:57:45 -0500

    Not finished

---
+++ script.js
@@ -0,0 +1,18 @@
+//const compChoice = getComputerChoice();
+//const userChoice = prompt("Select Rock, Paper, or Scissors");
+
+//function getComputerChoice{
+//    console.log(getComputerChoice);
+//}
+//function playRound(userChoice, compChoice){
+
+//}
+function getComputerChoice(Rock,Paper,Scissors);
+
+function playRound(playerSelection, computerSelection) {
+   if computer
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
